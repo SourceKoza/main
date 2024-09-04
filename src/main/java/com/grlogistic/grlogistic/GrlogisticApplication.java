214e3ab71ada3ffa2a0bbb367d27a4e91e8dec4f
Ramesh Pandey <rameshpandey@Rameshs-MacBook-Air.local>
2024-09-04 08:59:52 +0530

    initial commit

---
+++ src/main/java/com/grlogistic/grlogistic/GrlogisticApplication.java
@@ -0,0 +1,14 @@
+package com.grlogistic.grlogistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrlogisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrlogisticApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/grlogistic/grlogistic/GrlogisticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grlogistic.grlogistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrlogisticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
